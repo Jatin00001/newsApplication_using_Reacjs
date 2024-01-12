bded191bb2bff293ecb6dd29cd0a72f082930660
Jatin <datajatin90@gmail.com>
2024-01-12 18:40:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+// this is functional base component 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// export default App;
+
+// now we use class based component in this 
+//and search es7 react snippet    search on google 
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
+
+export default class App extends Component {
+  render() { // screen pr render krna kaam h render function ka
+    return (
+      <div>
+        <Navbar/>
+        <News/>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+// difference between function and class base 
+// methods management is easy qwe use this.variable
+
